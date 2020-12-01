497fddf168f68704f93ac9e71b1c78dbf342df80
Juanver <juan99verano@gmail.com>
2020-12-01 17:05:21 -0500

    Data y atributos dinámicos

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>SOY LA VISTA about</h1>
+    <p>{{this.data1}}</p>
+    <p>{{this.data2}}</p>
+    <img :alt='name' :src='url'>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      data1: 'soy la data 1',
+      data2: 1000,
+      data3: '',
+      name:'logo',
+      url:'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg'
+    }
+  }
+}
+</script>
