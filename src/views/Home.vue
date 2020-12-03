3e3c0e2d02617706f9275dbd52d31b35d4a67a81
Juanver <juan99verano@gmail.com>
2020-12-03 00:36:47 -0500

    V-show y V-model

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,31 @@
       <button class="btn btn-danger" @click="disminuir">Disminuir</button>
       <p>{{ this.numero }}</p>
     </div>
+    <div>
+      <p>{{this.fruta_seleccionada}}</p>
+      <form>
+        <input v-model="fruta_seleccionada" value="manzana" type="radio" id="manzana" name="fruta">
+        <label for="manzana">Manzana</label>
+        <input v-model="fruta_seleccionada" value="pera" type="radio" id="pera" name="fruta">
+        <label for="pera">Pera</label>
+        <input v-model="fruta_seleccionada" value="naranja" type="radio" id="naranja" name="fruta">
+        <label for="naranja">Naranja</label>
+
+       <div>
+          <input v-model="frutas" value="manzana" type="checkbox" id="manzana" name="manzana">
+          <label for="manzana">Manzana</label>
+          <input v-model="frutas" value="pera" type="checkbox" id="pera" name="pera">
+          <label for="pera">Pera</label>
+          <input v-model="frutas" value="naranja" type="checkbox" id="naranja" name="naranja">
+          <label for="naranja">Naranja</label>
+       </div>
+      </form>
+      <p>{{this.frutas}}</p>
+    </div>
+    <button class="btn btn-outline-primary" @click="mostrar">{{ show_box ? 'Ocultar' : 'Mostrar'}}</button>
+    <div v-show="show_box">
+      <p>Acá me muestro</p>
+    </div>
   </div>
 </template>
 
@@ -20,7 +45,10 @@ export default {
   },
   data(){
     return{
-      numero:0
+      numero:0,
+      frutas:[],
+      fruta_seleccionada:'',
+      show_box: true
     }
   },
   methods:{
@@ -29,6 +57,9 @@ export default {
     },
     disminuir(){
       this.numero = this.numero - 1
+    },
+    mostrar(){
+      this.show_box = !this.show_box
     }
   }
 }
