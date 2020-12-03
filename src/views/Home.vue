d770bc97ccc66feb52cc666d4725e32ecd0823ab
Juanver <juan99verano@gmail.com>
2020-12-02 23:50:40 -0500

    Introduccion a los metodos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,11 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HolaMundo msg="BIENVENIDO" number="Soy el número 1"/>
+    <div>
+      <button class="btn btn-primary" @click="aumentar">Aumentar</button>
+      <button class="btn btn-danger" @click="disminuir">Disminuir</button>
+      <p>{{ this.numero }}</p>
+    </div>
   </div>
 </template>
 
@@ -12,6 +17,19 @@ export default {
   name: 'Home',
   components: {
     HolaMundo
+  },
+  data(){
+    return{
+      numero:0
+    }
+  },
+  methods:{
+    aumentar(){
+      this.numero = this.numero + 1
+    },
+    disminuir(){
+      this.numero = this.numero - 1
+    }
   }
 }
 </script>
