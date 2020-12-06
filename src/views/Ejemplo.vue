d82f46744eec12bf7fd4079cdcbe3efdd0e129e0
Juanver <juan99verano@gmail.com>
2020-12-06 03:06:56 -0500

    propiedades computadas

--- src/views/Ejemplo.vue
+++ src/views/Ejemplo.vue
@@ -2,16 +2,6 @@
     <div class="container">
         <h1>{{mensaje}}</h1>
         <h2>{{ ejemplo }}</h2>
-        <div class="form-group">
-            <label for="exampleFormControlInput1">Email address</label>
-            <input type="text" v-model="number" class="form-control" id="exampleFormControlInput1" placeholder="">
-        </div>
-        <button class="btn btn-primary" @click="number++" >+</button>
-        <button class="btn btn-danger" @click="number--">-</button>
-
-        <div class="progress">
-            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
     </div>
 </template>
 <script>
