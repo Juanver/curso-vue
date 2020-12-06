7f6227e4ef7fb4b09b2fdd62cd75d4688435967d
Juanver <juan99verano@gmail.com>
2020-12-06 03:05:59 -0500

    propiedades computadas

--- src/views/Ejemplo.vue
+++ src/views/Ejemplo.vue
@@ -1,5 +1,31 @@
 <template>
     <div class="container">
-        <h1>Esta es la vista de EJMEPLO</h1>
+        <h1>{{mensaje}}</h1>
+        <h2>{{ ejemplo }}</h2>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Email address</label>
+            <input type="text" v-model="number" class="form-control" id="exampleFormControlInput1" placeholder="">
+        </div>
+        <button class="btn btn-primary" @click="number++" >+</button>
+        <button class="btn btn-danger" @click="number--">-</button>
+
+        <div class="progress">
+            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
     </div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            mensaje: 'Hola vue',
+            number: 0
+        }
+    },
+    computed:{
+        ejemplo(){
+            return this.mensaje.split('').reverse().join('');
+        }
+    }
+}
+</script>
