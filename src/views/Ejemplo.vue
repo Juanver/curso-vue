7d210927ade9eb85c09cc1397868b34cb8869064
Juanver <juan99verano@gmail.com>
2020-12-06 14:20:01 -0500

    Watcher

--- src/views/Ejemplo.vue
+++ src/views/Ejemplo.vue
@@ -2,6 +2,11 @@
     <div class="container">
         <h1>{{mensaje}}</h1>
         <h2>{{ ejemplo }}</h2>
+
+        <div>
+            <input type="text" v-model="nombre">
+            <h3>{{mensaje2}}</h3>
+        </div>
     </div>
 </template>
 <script>
@@ -9,12 +14,25 @@ export default {
     data(){
         return{
             mensaje: 'Hola vue',
-            number: 0
+            number: 0,
+            nombre: '',
+            mensaje2:'',
         }
     },
     computed:{
         ejemplo(){
             return this.mensaje.split('').reverse().join('');
+        }
+    },
+    watch:{
+        nombre: function(val){
+            console.log(val)
+            if (val == "juan"){
+                this.mensaje2 = "Hola, bienvenido " + val;
+            }
+            else{
+                this.mensaje2 = "";
+            }
         }
     }
 }
