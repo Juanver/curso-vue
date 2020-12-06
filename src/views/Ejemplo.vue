4e9b6ce108a4802d38d35cbcc148bd6034088533
Juanver <juan99verano@gmail.com>
2020-12-06 17:25:44 -0500

    Axios con promesas

--- src/views/Ejemplo.vue
+++ src/views/Ejemplo.vue
@@ -11,9 +11,23 @@
         <div>
             <h3>{{mensaje_mounted}}</h3>
         </div>
+        <h3>Axios</h3>
+        <div class="row">
+            <div v-for= "val in value" :key="val.id" class="col-4">
+                <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://placeimg.com/640/480/any" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Id: {{val.id}}</h5>
+                    <p class="card-text">Título: {{val.title }}</p>
+                    <a href="#" class="btn btn-success">OK</a>
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
     data(){
         return{
@@ -21,11 +35,17 @@ export default {
             number: 0,
             nombre: '',
             mensaje2:'',
-            mensaje_mounted: ''
+            mensaje_mounted: '',
+            value : []
         }
     },
     mounted () {
         this.mensaje_mounted = "Acá funciona el mounted"
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(respuesta => {
+            this.value = respuesta.data
+            console.log(this.value)
+        })
+        
     },
     computed:{
         ejemplo(){
